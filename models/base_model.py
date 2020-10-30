7e35a912cc0ad176e644e889444b2d8a4f237eaf
ZoltanMG <zoltanmoragarcia@gmail.com>
2020-10-30 14:45:13 +0000

    [Add] Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """
+this module contains the base class "BaseModel".
+
 """
 
 
@@ -9,18 +11,39 @@ from datetime import datetime
 
 class BaseModel:
     """
+    this is the base class that will be inherited in the other classes.
+
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         """
 
-        self.id = str(uuid.uuid4())
-        self.create_at = datetime.now()
-        self.update_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == 'id':
+                    self.id = value
+                elif key == '__class__':
+                    continue
+                elif key == 'created_at':
+                    self.created_at = datetime.strptime(value,
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == 'updated_at':
+                    self.updated_at = datetime.strptime(value,
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                elif key == 'name':
+                    self.name = value
+                elif key == 'my_number':
+                    self.my_number = value
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
+        This method returns a string in the following format:
+        "[<class name>] (<self.id>) <self .__ dict __>".
         """
 
         return '[{}] ({}) {}'.format(self.__class__.__name__,
@@ -29,16 +52,19 @@ class BaseModel:
 
     def save(self):
         """
+        in this method the date and time of
+        modification of the class is updated.
         """
 
-        self.update_at = datetime.now()
+        self.updated_at = datetime.now()
 
     def to_dict(self):
         """
+        in this method a dictionary is returned with the attributes of
+        the classes and the name of the class.
         """
-
-        self.create_at = self.create_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
-        self.update_at = self.update_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         my_dict = self.__dict__
+        my_dict['created_at'] = self.created_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
+        my_dict['updated_at'] = self.updated_at.strftime("%Y-%m-%dT%H:%M:%S.%f")
         my_dict['__class__'] = self.__class__.__name__
         return my_dict
