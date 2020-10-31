0a5a625a8c499f368cde4b4ff2353f5e8b3808b9
Camilo Barreiro <1845@holbertonschool.com>
2020-10-31 21:30:00 +0000

    Correction method save

--- models/base_model.py
+++ models/base_model.py
@@ -68,7 +68,7 @@ class BaseModel:
         the classes and the name of the class.
         """
 
-        my_dict = self.__dict__
+        my_dict = self.__dict__.copy()
         my_dict['created_at'] = self.created_at.isoformat()
         my_dict['updated_at'] = self.updated_at.isoformat()
         my_dict['__class__'] = self.__class__.__name__
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -20,17 +20,27 @@ class FileStorage:
 
         if obj is not None:
             keys = obj.__class__.__name__ + '.' + obj.id
-            to_dict = obj.to_dict()
-            self.__objects[keys] = to_dict
+            self.__objects[keys] = obj
 
     def save(self):
+        new_object = {}
         with open(self.__file_path, 'w', encoding="utf-8") as files:
-            abc = json.dumps(self.__objects)
+            for key in self.__objects:
+                new_object[key] = self.__objects[key].to_dict()
+            abc = json.dumps(new_object)
             files.write(abc)
 
     def reload(self):
         try:
             with open(self.__file_path, 'r', encoding="utf-8") as files:
                 self.__objects = json.load(files)
+                value = self.__objects['created_at']
+                self.__objects['created_at'] = datetime.strptime(value,
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+                value = self.__objects['created_at']
+                self.__objects['created_at'] = datetime.strptime(value,
+                                                        "%Y-%m-%dT%H:%M:%S.%f")
+            for key in self.__object:
+                print(self.__object[key])
         except:
             pass
